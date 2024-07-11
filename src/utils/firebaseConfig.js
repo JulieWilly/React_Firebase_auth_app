75759d8b82c7bfb510161be9b03a6b2299ff9623
Wilfred Muchire <logincoin40@gmail.com>
2024-07-11 16:12:54 +0300

    adding full application

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  apiKey: "AIzaSyCLEWdDf9uP7Bspym9Qjb-y8UXdoEAORb8",
+  authDomain:"1:156794948483:web:f0f3b84508e76c6ad204b6",
   projectId: "react-auth-57f9a",
   storageBucket: "react-auth-57f9a.appspot.com",
   messagingSenderId: "156794948483",
-  appId:process.env.APP_ID,
+  appId: "react-auth-57f9a.firebaseapp.com",
   measurementId: "G-VX3D1PST0J",
 };
 
