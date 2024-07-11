36c28516751892c2c19aec1dac7b35725bf60b08
Wilfred Muchire <logincoin40@gmail.com>
2024-07-11 15:36:35 +0300

    hidding import secret keys

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCLEWdDf9uP7Bspym9Qjb-y8UXdoEAORb8",
-  authDomain: "react-auth-57f9a.firebaseapp.com",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: "react-auth-57f9a",
   storageBucket: "react-auth-57f9a.appspot.com",
   messagingSenderId: "156794948483",
-  appId: "1:156794948483:web:f0f3b84508e76c6ad204b6",
+  appId:process.env.APP_ID,
   measurementId: "G-VX3D1PST0J",
 };
 
